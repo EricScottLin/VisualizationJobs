bc873111380a39b606a56374148e374afb9dcc70
Eric <mr_ericlin@163.com>
2023-12-19 12:56:47 +0800

    complete MainCrawl.py

--- spider/MainCrawl.py
+++ spider/MainCrawl.py
@@ -15,11 +15,12 @@ django.setup()
 from jobVisualization.models import JobInfo
 
 jobs = ['软件', '图像', '自然语言处理', '人工智能', '学习', '前端', '后端', '数据', '算法', '测试', '网络安全', '运维', 'UI', '区块链', '网络', '全栈',
-            '硬件', 'Java', 'C++', 'PHP', 'C#', '.NET', 'Hadoop', 'Python', 'Perl', 'Ruby', 'Nodejs', 'Go', 'Javascript',
-            'Delphi', 'jsp', 'sql', '软件工程师', '网络工程师', '数据分析师', '系统管理员', 'Java工程师', '前端工程师', 'Python开发', '人工智能工程师',
+        '硬件', 'Java', 'C++', 'PHP', 'C#', '.NET', 'Hadoop', 'Python', 'Perl', 'Ruby', 'Nodejs', 'Go', 'Javascript',
+        'Delphi', 'jsp', 'sql', '软件工程师', '网络工程师', '数据分析师', '系统管理员', 'Java工程师', '前端工程师', 'Python开发', '人工智能工程师',
         '数据库管理员', '项目经理', '前端工程师', '后端工程师', '后端工程师', '算法工程师', '测试工程师', '运维工程师', 'DevOps工程师', '产品经理',
         'UI/UX设计师', '信息安全分析师', '全栈开发', '软件架构师', '数据工程师', '云计算工程师', '人工智能研究员', '嵌入式软件工程师', 'Web工程师', '游戏开发',
         '大数据工程师', '区块链工程师', '机器学习工程师', '物联网工程师', '信息技术经理', '网络安全工程师']
+
 
 def startBrowser():
     service = Service('./chromedriver')
@@ -239,6 +240,7 @@ class crawl(object):
 
 if __name__ == '__main__':
     for j in jobs:
+        print(f"爬取关键字为'{j}'的岗位信息")
         crawlObj = crawl(j, 1)
         init()
         crawlObj.main(30)
