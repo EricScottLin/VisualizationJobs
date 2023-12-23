4f457082030c531294dee7efcaf8a390d2e619e5
EricScottLin <mr_ericlin@163.com>
2023-12-23 10:36:12 +0800

    finale complete the code

--- static/js/right2.js
+++ static/js/right2.js
@@ -1,6 +1,7 @@
 var mySwiper = new Swiper('.swiper', {
-    direction: 'vertical', // 垂直切换选项
+    direction: 'horizontal', // 垂直切换选项
     loop: true, // 循环模式选项
+    autoplay: true,
 
     // 如果需要分页器
     pagination: {
