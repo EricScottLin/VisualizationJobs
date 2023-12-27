61b74c8bc6f1531142637bc14869b6ef17e53f86
Eric <mr_ericlin@163.com>
2023-12-27 20:02:38 +0800

    Optimize the code structure

--- spider/MainCrawl.py
+++ spider/MainCrawl.py
@@ -14,33 +14,52 @@ django.setup()
 
 from jobVisualization.models import JobInfo
 
-# '软件', '图像', '自然语言处理', '人工智能', '学习', '前端', '后端', '数据', '算法', '测试', '网络安全', '运维', 'UI', '区块链', '网络', '全栈',
-#         '硬件', 'Java', 'C++', 'PHP', 'C#', '.NET', 'Hadoop', 'Python', 'Perl', 'Ruby', 'Nodejs', 'Go', 'Javascript',
-#         'Delphi', 'jsp', 'sql', '软件工程师', '网络工程师', '数据分析师', '系统管理员', 'Java工程师', '前端工程师', 'Python开发',
-#         '人工智能工程师', '数据库管理员', '项目经理', '算法工程师', '测试工程师', '运维工程师', 'DevOps工程师', '产品经理',
-#         'UI/UX设计师', '信息安全分析师', '全栈开发', '软件架构师', '数据工程师', '云计算工程师', '人工智能研究员', '嵌入式软件工程师', 'Web工程师',
-#         '大数据工程师', '区块链工程师', '机器学习工程师', '物联网工程师', '信息技术经理', '网络安全工程师', '游戏开发'
-
-jobs = ['前端', '后端', 'Python', 'Java', 'JavaScript', '人工智能', '数据分析师', '全栈', '软件测试', '机器学习', '数据库', '大数据', '云计算',
-        '区块链工程师', '网络安全工程师', '物联网工程师', '嵌入式软件工程师', 'vue', '游戏开发', '运维', 'Hadoop', 'Nodejs']
+jobs = ['前端', '后端', '全栈', '软件工程师', '网络工程师', '数据分析师', '系统管理员', 'Java工程师', '前端工程师']
 
 
 def startBrowser():
     service = Service('./chromedriver')
     options = webdriver.ChromeOptions()
-    # options.add_experimental_option("excludeSwitches", ['enable-automation'])
     options.add_experimental_option("debuggerAddress", "localhost:9222")
     return webdriver.Chrome(service=service, options=options)
 
 
+def init():
+    if not os.path.exists('temp.csv'):
+        with open('temp.csv', 'a', encoding='utf-8', newline='') as wf:
+            writer = csv.writer(wf)
+            writer.writerow([
+                'title',  # 岗位名字
+                'address',  # 地址
+                'type',  # 岗位
+                'education',  # 学历
+                'workExperience',  # 工作经验
+                'workTags',  # 工作标签
+                'salary',  # 薪资
+                'salaryMonth',  # 年底多薪
+                'companyTags',  # 公司福利
+                'hrWork',  # HR职位
+                'hrName',  # HR姓名
+                'practice',  # 实习生
+                'companyTitle',  # 公司名字
+                'companyAvatar',  # 公司头像
+                'companyType',  # 公司性质
+                'companyStatus',  # 公司状态
+                'companyScale',  # 规模
+                'detailUrl',  # 岗位详情页
+                'companyUrl',  # 公司详情页
+                'dist',  # 行政区
+            ])
+
+
 def save_to_csv(rowData):
-    with open('temp0.csv', 'a', newline='', encoding='utf-8') as wf:
+    with open('temp.csv', 'a', newline='', encoding='utf-8') as wf:
         writer = csv.writer(wf)
         writer.writerow(rowData)
 
 
 def clean_data():
-    df = pd.read_csv('temp0.csv')
+    df = pd.read_csv('temp.csv')
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
     df['salaryMonth'] = df['salaryMonth'].map(lambda x: x.replace('薪', ''))
@@ -75,34 +94,6 @@ def save_to_mysql():
             companyUrl=job[18],
             dist=job[19],
         )
-
-
-def init():
-    if not os.path.exists('temp0.csv'):
-        with open('temp0.csv', 'a', encoding='utf-8', newline='') as wf:
-            writer = csv.writer(wf)
-            writer.writerow([
-                'title',  # 岗位名字
-                'address',  # 地址
-                'type',  # 岗位
-                'education',  # 学历
-                'workExperience',  # 工作经验
-                'workTags',  # 工作标签
-                'salary',  # 薪资
-                'salaryMonth',  # 年底多薪
-                'companyTags',  # 公司福利
-                'hrWork',  # HR职位
-                'hrName',  # HR姓名
-                'practice',  # 实习生
-                'companyTitle',  # 公司名字
-                'companyAvatar',  # 公司头像
-                'companyType',  # 公司性质
-                'companyStatus',  # 公司状态
-                'companyScale',  # 规模
-                'detailUrl',  # 岗位详情页
-                'companyUrl',  # 公司详情页
-                'dist',  # 行政区
-            ])
 
 
 class crawl(object):
